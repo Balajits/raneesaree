5f69ddb8c328c392247779e039504a04fe9671fa
Balaji Ts <50311511+Balajits@users.noreply.github.com>
2024-07-03 20:48:14 +0530

    Add files via upload

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,47 +1,30 @@
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+    scrollFunction();
+};
+
+
+var mybutton = document.getElementById("btn-back-to-top");
+var navbar = document.getElementById("navbar");
 
 function scrollFunction() {
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-
-        var ele = document.getElementById("navbar");
-        ele.classList.add("shadow-lg");
-        ele.classList.remove('bg-trans');
-        ele.classList.add('bg-light');
-
+        navbar.classList.add("shadow-lg");
+        navbar.classList.remove('bg-trans');
+        navbar.classList.add('bg-light');
+        mybutton.style.display = "block";
     } else {
-
-        var ele = document.getElementById("navbar");
-        ele.classList.remove("shadow-lg");
-        ele.classList.add('bg-trans');
-        ele.classList.remove('bg-light');
+        navbar.classList.remove("shadow-lg");
+        navbar.classList.add('bg-trans');
+        navbar.classList.remove('bg-light');
+        mybutton.style.display = "none";
     }
 }
 
-
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollToTop();
-};
-
-function scrollToTop() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
-
 
