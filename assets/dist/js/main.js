d6aeefd256ca5d6e53a06ea9fee98ecfd1224892
Balaji Ts <50311511+Balajits@users.noreply.github.com>
2024-07-03 12:47:42 +0530

    Add files via upload

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -7,7 +7,7 @@ function scrollFunction() {
         ele.classList.add("shadow-lg");
         ele.classList.remove('bg-trans');
         ele.classList.add('bg-light');
-        
+
     } else {
 
         var ele = document.getElementById("navbar");
@@ -16,3 +16,32 @@ function scrollFunction() {
         ele.classList.remove('bg-light');
     }
 }
+
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollToTop();
+};
+
+function scrollToTop() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
